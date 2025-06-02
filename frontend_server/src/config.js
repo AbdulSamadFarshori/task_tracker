d63e8e09d087292d31fc76dad65ae94329894286
sumir <sumir40@gmail.com>
2025-06-02 13:42:45 +0530

    finalize --version 6

--- frontend_server/src/config.js
+++ frontend_server/src/config.js
@@ -1,2 +1,3 @@
-export const serverURL = "http://13.201.129.250";
+// export const serverURL = "https://13.201.129.250";
+export const serverURL = "https://abdulfarshori.xyz"
 export const localServerURL = "http://127.0.0.1:5000";
