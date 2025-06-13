8cd4efa9ad0639b215bc848a35058cdbffa21841
sumir <sumir40@gmail.com>
2025-06-14 00:14:45 +0530

    finally final --version 5

---
+++ frontend_server/src/components/Input.js
@@ -0,0 +1,8 @@
+import { Fragment } from "react"
+
+export default function InputField({labelFor, title, type, idName, name, value, onChange}){
+   return(<Fragment> 
+   <label for={idName}>{title}</label>
+   <input type={type} id={idName} name={name} value={value} onChange={onChange} required />
+   </Fragment>) 
+}
