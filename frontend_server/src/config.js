c096e1673301a6ef6d88b116bbd4b9d3515d2687
sumir <sumir40@gmail.com>
2025-06-26 01:51:40 +0530

    final present -3

--- frontend_server/src/config.js
+++ frontend_server/src/config.js
@@ -1,6 +1,6 @@
 // export const serverURL = "https://13.201.129.250";
 export const serverURL = "https://abdulfarshori.xyz/api";
 export const localServerURL = "http://localhost:5000/api";
-export const BaseURL = localServerURL;
+export const BaseURL = serverURL;
 
 export const key = process.env.REACT_APP_CLIENT_ID
